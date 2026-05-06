feca0caff6b20bf587b3b6fad04c3e50b17ed097
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-06 23:23:42 +0530

    Maximum Consecutive Ones

--- 03_Arrays/Ques/01.cpp
+++ 03_Arrays/Ques/01.cpp
@@ -1,26 +1,25 @@
-//Reverse an Array
-//https://www.geeksforgeeks.org/problems/reverse-an-array/1
-
+//Maximum Consecutive Ones
+//https://leetcode.com/problems/max-consecutive-ones/
+//+++++++++++OPTIMAL++++++++++
 /*
-2 POINTER APPROACH
-class Solution {
-  public:
-    void reverseArray(vector<int> &arr) {
-        // code here
-        int left = 0;
-        int right = arr.size()-1;
-        while(left<right){
-            swap(arr[left],arr[right]);
-            left++;
-            right--;
-        }
+ek count rkho aur maximum count ko update krte jao jab bhi 1 mile aur jab 0 mile to count ko reset krdo
+
+int maxi=0;
+int count=0;
+for(int i=0;i<n;i++){
+    if(arr[i]==1){
+        count++;
+        maxi=max(maxi,count);
     }
-};
-TC => O(N)
-SC => O(1)
+    else{
+        count=0;
+    }
+    }
+}
+return maxi;
+
+TC: O(n)
+SC: O(1)
 */
 
-/*
-STL SHORTCUT
-reverse(arr.begin(), arr.end());
-*/
+
