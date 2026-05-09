f39b9ece20dc7e8bcdebb10cda6cb516dcaad1bc
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-09 16:53:01 +0530

    Job Sequencing Problem

--- 12_Greedy/03.cpp
+++ 12_Greedy/03.cpp
@@ -65,3 +65,81 @@ class Solution {
     }
 };
 */
+
+//+++++OPTIMAL++++++++
+/*
+✅ Optimized Approach (DSU / Disjoint Set)
+
+We use:
+
+Disjoint Set Union (Union Find)
+
+to quickly find the latest free slot.
+
+class Solution {
+  public:
+
+    // Find parent (available slot)
+    int findParent(int node, vector<int>& parent) {
+
+        // Path compression
+        if(parent[node] == node)
+            return node;
+
+        return parent[node] = findParent(parent[node], parent);
+    }
+
+    vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) {
+
+        int n = deadline.size();
+
+        // Store jobs as {profit, deadline}
+        vector<pair<int,int>> jobs;
+
+        for(int i = 0; i < n; i++) {
+            jobs.push_back({profit[i], deadline[i]});
+        }
+
+        // Sort jobs by descending profit
+        sort(jobs.begin(), jobs.end(), greater<pair<int,int>>());
+
+        // Find maximum deadline
+        int maxDeadline = *max_element(deadline.begin(), deadline.end());
+
+        // DSU parent array
+        vector<int> parent(maxDeadline + 1);
+
+        // Initially every slot is free
+        for(int i = 0; i <= maxDeadline; i++) {
+            parent[i] = i;
+        }
+
+        int countJobs = 0;
+        int totalProfit = 0;
+
+        // Process all jobs
+        for(auto job : jobs) {
+
+            int currProfit = job.first;
+            int currDeadline = job.second;
+
+            // Find latest available slot
+            int availableSlot = findParent(currDeadline, parent);
+
+            // If slot available
+            if(availableSlot > 0) {
+
+                // Occupy slot
+                parent[availableSlot] =
+                    findParent(availableSlot - 1, parent);
+
+                countJobs++;
+
+                totalProfit += currProfit;
+            }
+        }
+
+        return {countJobs, totalProfit};
+    }
+};
+*/
