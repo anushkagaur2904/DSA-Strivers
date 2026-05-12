26939c00c00cbe7da2f4951003daa4bea2f29dc4
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-12 18:34:00 +0530

    Peak Element

---
+++ 04_Binary Search/Ques/12.cpp
@@ -0,0 +1,47 @@
+//Peak Element
+//https://leetcode.com/problems/find-peak-element/description/
+
+/*
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        int n = arr.size();
+        int low =0;
+        int high = n-1;
+         // Edge case: single element
+        if(n == 1)
+            return 0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(mid>0 && mid<n-1){
+                if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                    return mid;
+                }
+                else if(arr[mid-1]>arr[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1; 
+                }
+            }
+            else if(mid==0){
+                if(arr[mid]>arr[mid+1]){
+                    return 0;
+                }
+                else{
+                    return 1;
+                }
+            }
+            else if(mid==n-1){
+                if(arr[n-1]>arr[n-2]){
+                    return n-1;
+                }
+                else{
+                    return n-2;
+                }
+            }
+        }
+        return -1;
+    }
+};
+*/
