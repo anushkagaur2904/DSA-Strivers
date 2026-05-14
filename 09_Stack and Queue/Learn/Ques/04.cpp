5965e57156c1846ecee780ee7bd2972d7331e28d
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-14 13:01:16 +0530

    NSR

---
+++ 09_Stack and Queue/Learn/Ques/04.cpp
@@ -0,0 +1,36 @@
+//Nearest Smaller to Right
+
+/*
+class Solution {
+  public:
+    vector<int> nextSmallerRight(vector<int>& arr) {
+        // code here
+        vector<int> v;
+        stack<int> s;
+        int n = arr.size();
+        
+        for(int i=n-1;i>=0;i--){
+            if(s.size()==0){
+                v.push_back(-1);
+            }
+            else if(s.size()>0 && s.top()<arr[i]){
+                v.push_back(s.top());
+            }
+            else if(s.size()>0 && s.top()>=arr[i]){
+                while(s.size()>0 && s.top()>=arr[i]){
+                    s.pop();
+                }
+                if(s.size()==0){
+                    v.push_back(-1);
+                }
+                else{
+                    v.push_back(s.top());
+                }
+            }
+            s.push(arr[i]);
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
+*/
