d8d2f82349bacb5d74bc11271d130bf2cf4caba4
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-14 11:35:12 +0530

    NGR

---
+++ 09_Stack and Queue/Learn/Ques/01.cpp
@@ -0,0 +1,38 @@
+//Next Largest Element (NGR)
+//https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+/*
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        vector<int> v;
+        stack<int> s;
+        int n = arr.size();
+        
+        for(int i=n-1;i>=0;i--){
+            if(s.size()==0){
+                v.push_back(-1);
+            }
+            else if(s.size()>0 && s.top()>arr[i]){
+                v.push_back(s.top());
+            }
+            else if(s.size()>0 && s.top()<=arr[i]){
+                while(s.size()>0 && s.top()<=arr[i]){
+                    s.pop();
+                }
+                if(s.size()==0){
+                    v.push_back(-1);
+                }
+                else{
+                    v.push_back(s.top());
+                }
+            }
+            s.push(arr[i]);
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
+*/
+
