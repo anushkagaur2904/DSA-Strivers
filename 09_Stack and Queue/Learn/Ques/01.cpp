92ed93a8f7bf7061752e7e8e81dbc7a08b29b507
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-17 18:25:41 +0530

    NGR

--- 09_Stack and Queue/Learn/Ques/01.cpp
+++ 09_Stack and Queue/Learn/Ques/01.cpp
@@ -36,3 +36,60 @@ class Solution {
 };
 */
 
+
+//LEETCODE PROBLEM
+//https://leetcode.com/problems/next-greater-element-i/description/
+
+/*
+class Solution {
+public:
+
+    vector<int> nextGreaterElement(vector<int>& nums1,
+                                   vector<int>& nums2) {
+
+        stack<int> s;
+
+        unordered_map<int,int> mp;
+
+        // Traverse nums2 from right to left
+        for(int i = nums2.size() - 1;
+            i >= 0;
+            i--) {
+
+            // Remove smaller/equal elements
+            while(!s.empty() &&
+                  s.top() <= nums2[i]) {
+
+                s.pop();
+            }
+
+            // No greater element exists
+            if(s.empty()) {
+
+                mp[nums2[i]] = -1;
+            }
+
+            // Next greater element found
+            else {
+
+                mp[nums2[i]] = s.top();
+            }
+
+            // Push current element
+            s.push(nums2[i]);
+        }
+
+        vector<int> ans;
+
+        // Get answers for nums1
+        for(int i = 0;
+            i < nums1.size();
+            i++) {
+
+            ans.push_back(mp[nums1[i]]);
+        }
+
+        return ans;
+    }
+};
+*/
