9fa463c6fbcb9f8facd3e2cec574e7f2ae820659
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-17 11:24:07 +0530

    min stack with O(1) space

--- 09_Stack and Queue/Learn/Ques/09.cpp
+++ 09_Stack and Queue/Learn/Ques/09.cpp
@@ -27,3 +27,95 @@ int getMin(){
     return ss.top();
 }
 */
+
+//Min element in stack with O(1) space
+//https://leetcode.com/problems/min-stack/description/
+
+/*
+class MinStack {
+public:
+
+    long long minEle;
+
+    stack<long long> s;
+
+    MinStack() {
+
+    }
+
+    void push(int val) {
+
+        // First element
+        if(s.empty()) {
+
+            s.push(val);
+
+            minEle = val;
+        }
+
+        else {
+
+            // Normal push
+            if(val >= minEle) {
+
+                s.push(val);
+            }
+
+            // Encoded push
+            else {
+
+                s.push(2LL * val - minEle);
+
+                minEle = val;
+            }
+        }
+    }
+
+    void pop() {
+
+        if(s.empty())
+            return;
+
+        // Normal value
+        if(s.top() >= minEle) {
+
+            s.pop();
+        }
+
+        // Encoded value
+        else {
+
+            minEle =
+            2LL * minEle - s.top();
+
+            s.pop();
+        }
+    }
+
+    int top() {
+
+        if(s.empty())
+            return -1;
+
+        // Normal value
+        if(s.top() >= minEle) {
+
+            return s.top();
+        }
+
+        // Encoded value
+        else {
+
+            return minEle;
+        }
+    }
+
+    int getMin() {
+
+        if(s.empty())
+            return -1;
+
+        return minEle;
+    }
+};
+*/
