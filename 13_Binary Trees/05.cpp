4d690fad2010196241e084e8d5486198f41c0a6f
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-22 14:07:09 +0530

    Iterative Inorder Traversal in Binary Tree

---
+++ 13_Binary Trees/05.cpp
@@ -0,0 +1,42 @@
+//Iterative Inorder Traversal in Binary Tree 
+//https://www.geeksforgeeks.org/problems/inorder-traversal-iterative/1
+
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+/*
+class Solution {
+  public:
+    vector<int> inOrder(Node* root) {
+        // code here
+        stack<Node*> st;
+        Node* node = root;
+        vector<int> inorder;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty()==true) break;
+                node = st.top();
+                st.pop();
+                inorder.push_back(node->data);
+                node = node->right;
+            }
+        }
+        return inorder;
+    }
+};
+*/
