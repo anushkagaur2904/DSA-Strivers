393013584c6f960b1ea005bf1f63f881a4eb4208
Anushka Gaur <anushkagaur52@gmail.com>
2026-06-04 20:58:20 +0530

    Print root to leaf path in BT

---
+++ 13_Binary Trees/21.cpp
@@ -0,0 +1,50 @@
+//Print root to leaf path in BT
+
+/*
+// Class containing the solution
+class Solution {
+public:
+    // Function to find the path from root to node with value x
+    bool getPath(TreeNode* root, vector<int>& arr, int x) {
+        // If current node is NULL, return false
+        if (!root) {
+            return false;
+        }
+
+        // Add current node's value to the path
+        arr.push_back(root->val);
+
+        // If current node's value is equal to x, return true
+        if (root->val == x) {
+            return true;
+        }
+
+        // Recursively search in left or right subtree
+        if (getPath(root->left, arr, x) ||
+            getPath(root->right, arr, x)) {
+            return true;
+        }
+
+        // If not found, backtrack and remove current node
+        arr.pop_back();
+        return false;
+    }
+
+    // Function to get the path from root to node with value B
+    vector<int> solve(TreeNode* A, int B) {
+        // Vector to store the path
+        vector<int> arr;
+
+        // If root is NULL, return empty path
+        if (A == NULL) {
+            return arr;
+        }
+
+        // Call helper function to fill the path
+        getPath(A, arr, B);
+
+        // Return the resulting path
+        return arr;
+    }
+};
+*/
