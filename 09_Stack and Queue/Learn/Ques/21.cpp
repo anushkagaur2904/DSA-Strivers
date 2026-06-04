dbfd891c7b87eaa52aa2bb6316ba28dd8fc498dc
Anushka Gaur <anushkagaur52@gmail.com>
2026-06-05 00:23:45 +0530

    max width

--- 09_Stack and Queue/Learn/Ques/21.cpp
+++ 09_Stack and Queue/Learn/Ques/21.cpp
@@ -5,34 +5,42 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root){
-            return 0;
-        }
-        int ans =0;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
+        if (!root) return 0;
 
-        while(!q.empty()){
+        long long ans = 0;
+
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root, 0});
+
+        while (!q.empty()) {
+
             int size = q.size();
-            int mmin = q.front().second;//to make id starting from zero
-            int first,last;
+            long long mmin = q.front().second;
 
-            for(int i=0;i<size;i++){
-                int cur_id = q.front().second - mmin;
+            long long first, last;
+
+            for (int i = 0; i < size; i++) {
+
+                long long cur_id = q.front().second - mmin;
                 TreeNode* node = q.front().first;
                 q.pop();
-                if(i==0) first = cur_id;
-                if(i==size-1) last = cur_id;
-                if(node->left){
-                    q.push({node->left, cur_id*2 +1 });
+
+                if (i == 0) first = cur_id;
+                if (i == size - 1) last = cur_id;
+
+                if (node->left) {
+                    q.push({node->left, 2 * cur_id + 1});
                 }
-                if(node->right){
-                    q.push({node->right, cur_id*2 +2 });
+
+                if (node->right) {
+                    q.push({node->right, 2 * cur_id + 2});
                 }
             }
-            ans = max(ans,last-first+1);
+
+            ans = max(ans, last - first + 1);
         }
-        return ans;
+
+        return (int)ans;
     }
 };
 */
